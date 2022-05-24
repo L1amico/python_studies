e5cb2f02f23a9ea441cd26864b1cba0a708681dc
Islombek <106137955+L1amico@users.noreply.github.com>
2022-05-24 11:49:18 +0500

    if darslar bor kurishingiz mumkin

---
+++ if_elif_else.py
@@ -0,0 +1,42 @@
+
+#yosh = int(input('How old are u ?\n>>>>'))
+#if yosh<=4:
+#  cost = 0.50 
+#elif	yosh<=12:
+#	cost= 1 
+#elif	yosh<=18:
+#	cost = 5 
+#else:
+#  cost= 10 
+
+#print(f"entering for u my man {cost}$")  
+
+##### or command bn ishlash
+#kun = input("what is a day today ?\n>>>>")
+#if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+#	print('take a relax.')
+#else:
+#	print("get up go work now!")
+
+
+### and operatori bilan ikkala shartning bajarilishini kurishmz mumkin agar ikkalar shart
+### if kun.lower()=='yakshanba' and harorat>=30: true bulsa bunda bizga
+### command aniq javobni kursatadi
+
+#kun = input("what is a day today ?\n>>>>")
+#harorat = float(input("temperature of a weather?\n>>>>"))
+
+#if kun.lower()=='yakshanba' and harorat>=30:
+#	print("chomilshga gooo.")
+#elif kun.lower()=='yakshanba' and harorat<=30:
+#	print("chomilish atmen bollar!")
+
+
+
+#kun = input("what is a day today ?\n>>>>")
+#harorat = float(input("temperature of a weather?\n>>>>"))
+
+#if (kun.lower()=='yakshanba' or kun.lower()=='shanba') and harorat>=30:
+#	print("chomilshga gooo.")
+#elif (kun.lower()=='yakshanba' or kun.lower()=='shanba') and harorat<=30:
+#	print("chomilish atmen bollar!")
